cbc9b910757a220f6918616c1779049b6fc8cac9
chahd <chahd@Al-Hares>
2019-04-23 02:43:04 +0200

    Read all .mat files in a folder, cut them at "sot" and save the results

---
+++ Cut_at_sot.m
@@ -0,0 +1,43 @@
+% time in seconds after sot
+clear
+time = 5;
+% select the folder with the data and get a structure with all .mat files
+disp("Select Folder with Data");
+DataFolder = uigetdir(pwd);
+Files = dir(fullfile(DataFolder, '*.mat'));
+
+%Select the Output Folder
+disp("Select Output Folder");
+OutFolder = uigetdir(pwd);
+
+for k=1:length(Files)
+    %CurrentFile = Files(k).name;
+    CurrentFile = fullfile(Files(k).folder, Files(k).name);
+    load (CurrentFile)
+    [~,name,~] = fileparts(CurrentFile);
+    StartPoint = fs * sot;
+    % Generate random number between 5 and 30
+    ran = randi([5, 30]);
+    % Chekc if outside range
+    if (sot - ran)*fs < 0
+       ran = sot; 
+    end
+    FsAfterSOT = (sot+time)*fs;
+    FsBeforeSOT = (sot-ran)*fs;
+    
+    % take data from all channels before and after sot
+    DataAfterSOT = d(StartPoint:FsAfterSOT,:);
+    DataBeforeSOT = d(FsBeforeSOT:StartPoint,:);
+    
+    % resutling channels with seizure, according to the give soz
+    DataWithSeizure = DataAfterSOT(:,soz); % maybe here iz instead of soz?
+    
+    % write data
+    out1 = fullfile(OutFolder, strcat(name, '_before' ,'.mat'));
+    out2 = fullfile(OutFolder, strcat(name, '_after' ,'.mat'));
+    out3 = fullfile(OutFolder, strcat(name, '_seizure' ,'.mat'));
+    save(out1, 'DataBeforeSOT');
+    save(out2, 'DataAfterSOT');
+    save(out3, 'DataWithSeizure');
+end
+
